ab5403d2caa75c9201eb1c0f5d4eaa79df00ab24
jeroenvdkolk <jeroenvdkolk@gmail.com>
2015-01-23 15:23:12 +0100

    models

--- src/model/TableInfo.java
+++ src/model/TableInfo.java
@@ -15,6 +15,51 @@ public class TableInfo {
     Round round;
     Tournament tournament;
     Player player;
-    boolean winner;
+    //enum winner;
     
+    public TableInfo(Table table, Round round, Tournament tournament, Player player) {
+        this.table = table;
+        this.round = round;
+        this.tournament = tournament;
+        this.player = player;
+        //this.winner = winner;
+        
+    }
+
+    public Table getTable() {
+        return table;
+    }
+
+    public void setTable(Table table) {
+        this.table = table;
+    }
+    
+    public Round getRound() {
+        return round;
+    }
+    
+    public void setRound(Round round) {
+        this.round = round;
+    }
+
+    public Tournament getTournament() {
+        return tournament;
+    }
+    
+    public void setTournament(Tournament tournament) {
+        this.tournament = tournament;
+    }
+    
+    public Player getPlayer() {
+        return player;
+    }
+    
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public String toString(){
+        return "" + player.getId() + "   " + round.getRoundNumber() + "   " + table.getTableNumber() + "   " + tournament.getTournamentId() + "   ";
+    }
 }
